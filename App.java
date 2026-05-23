ce666b715970d86bc04203de1dbf7c3882e63ea1
Hemanthmatthi <matthihemanth2004@gmail.com>
2026-05-23 10:24:26 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,7 @@
+// Save this file as Main.java
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println prints text to the console
+        System.out.println("Hello, World!");
+    }
+}
